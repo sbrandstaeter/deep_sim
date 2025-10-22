311ea0f6142d72823bdf7ca0b82806f93e79e83d
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2025-10-22 09:11:35 +0200

    return topology to allow computing statistics and plotting

--- pydeep_sim/rough_surface/patches_generation.py
+++ pydeep_sim/rough_surface/patches_generation.py
@@ -117,7 +117,7 @@ def patches_generation(
             delimiter=";",
         )
         print("Final aggregated surface was created successfully")
-        return z_patch
+        return z_patch, topology
     except Exception as e:
         print(f"An error occurred: {e}")
 
