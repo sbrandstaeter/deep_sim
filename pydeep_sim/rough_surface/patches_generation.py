819bfecf8f737c0c9b150d9ea9e9751a19224c04
Jacopo Bonari <jacopo.bonari@unibw.de>
2025-11-20 14:22:24 +0000

    Complete surface patch generation and pre-process.

--- pydeep_sim/rough_surface/patches_generation.py
+++ pydeep_sim/rough_surface/patches_generation.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import scipy.stats as st
 from scipy.special import erf
+from skspatial.objects import Plane, Points
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -87,11 +88,85 @@ def patches_generation(
         z_raw[k] = z_raw[k][:-1, :-1]
         h_max.append(np.max(z_raw[k]))
 
+    # -1- ######################################################################
+    #
+    # Remove average slope from all the patch surfaces, because the reviewer
+    # asks so.
+    #
+    ############################################################################
+
+    dx = l/2**resolution
+    dy = dx
+    x = np.linspace(dx/2, l-dx/2, 2**resolution)
+    y = np.linspace(dy/2, l-dy/2, 2**resolution)
+    X, Y = np.meshgrid(x, y)
+
+    for k in range(n_iter):
+
+        # Rearrange surface points to perform plane fitting:
+        surface_array = Points(
+            np.vstack((X.ravel(), Y.ravel(), z_raw[k].ravel())).T)
+        fitting_plane = Plane.best_fit(surface_array)
+
+        # Coefficients of the fitting plane a*x+b*y+c*z+d == 0
+        (a, b, c, d) = fitting_plane.cartesian()
+
+        # Subtraction of fitting plane from original surface:
+        z_raw[k] = z_raw[k]-(-1/c*(a*X+b*Y+d))
+
+        # By performing this operation, a small tilt remains, due to
+        # linearization. This can be shown doing a new plane fitting, that
+        # highlights how a residual angle is present. To obtain a rough surface
+        # whose mean plane is perfectly even, a rotation must be performed
+        # around the first two Euler angles of the unit normal of the mean
+        # plane, expressed in spherical coordinates. Out of curiosity, this can
+        # be accomplished by:
+
+        """
+        # Plane normal unit vector:
+        n = fitting_plane.normal
+
+        # Original polar and azimuth angles in spherical coordinates:
+        th = np.acos(n[2])
+        phi = np.atan2(n[1], n[0])
+
+        # Rotation of -phi about z-axis:
+        Rphi = np.array([[np.cos(-phi), -np.sin(-phi), 0.0],
+                        [np.sin(-phi),  np.cos(-phi), 0.0],
+                        [0.0,           0.0, 1.0]])
+
+        # Rotation of -th about y-axis:
+        Rth = np.array([[np.cos(-th),  0.0, np.sin(-th)],
+                        [0.0,          1.0,         0.0],
+                        [-np.sin(-th), 0.0, np.cos(-th)]])
+
+        # Compund 3D rotation operation:
+        R = Rth@Rphi
+        surface_rot = surface_array@R.T #Extended expression is: (R@surface_rot.T).T
+
+        fitting_plane_rot = Plane.best_fit(surface_rot)
+        (a, b, c, d) = fitting_plane_rot.cartesian()
+        print(th, phi, a, b, c, d)
+
+        # In this case, the surface should be then mapped back to NxN format.
+        """
+
+    # -2- ######################################################################
+    #
     # Remove mean plane from all the patch surfaces, since asperities with same
     # height from different surfaces must come into contact with same separation
-    # plane
+    # plane.
+    #
+    ############################################################################
+
     for k in range(n_iter):
         z_raw[k] -= np.mean(z_raw[k])
+
+    # -3- ######################################################################
+    #
+    # Join the patches together to generate the final patchwork surface
+    #
+    ############################################################################
 
     """
     # Small test to see if things are coming out as expected
@@ -103,16 +178,57 @@ def patches_generation(
     """
 
     patches_per_side = int(np.sqrt(n_iter))
-    z_patch = np.block(
+    z_patch_raw = np.block(
         [
             [z_raw[i * patches_per_side + j] for j in range(patches_per_side)]
             for i in range(patches_per_side)
         ]
     )
 
-    # delete_content(path_to_patches)
+    # -4- ######################################################################
+    #
+    # Smooth the "fault lines" to avoid undesired "canyoning" effect
+    #
+    ############################################################################
+
+    z_patch = z_patch_raw.copy()
+
+    th = 0.8
+    for i in range(N):
+
+        i_top = n*i
+        i_bottom = n*(i+1)-1
+
+        if i_top > 0:
+            z_patch[i_top, :] = th * \
+                z_patch_raw[i_top, :] + (1-th)*z_patch_raw[i_top-1, :]
+        if i_bottom < n*N-1:
+            z_patch[i_bottom, :] = th * \
+                z_patch_raw[i_bottom, :] + (1-th)*z_patch_raw[i_bottom+1, :]
+
+    for j in range(N):
+
+        j_left = n*j
+        j_right = n*(j+1)-1
+
+        if j_left > 0:
+            z_patch[:, j_left] = th * \
+                z_patch_raw[:, j_left] + (1-th)*z_patch[:, j_left-1]
+        if j_right < n*N-1:
+            z_patch[:, j_right] = th * \
+                z_patch_raw[:, j_right] + (1-th)*z_patch_raw[:, j_right+1]
+
+    # -5- ######################################################################
+    #
+    # Set minimum height to 0.0, so that lowest point is in contact without
+    # exterting force.
+    #
+    ############################################################################
 
     z_patch -= np.min(z_patch)
+
+    # delete_content(path_to_patches)
+
     # Save final patchwork surface and store it in surface database
     try:
         np.savetxt(
@@ -140,7 +256,8 @@ def plot_probability_density(z, num_patches, path_to_figure):
         linestyle="--",
         color="k",
     )
-    plt.axvline(mean_z, linestyle=":", color="k", linewidth=0.75, label="$\mu$")
+    plt.axvline(mean_z, linestyle=":", color="k",
+                linewidth=0.75, label="$\mu$")
     plt.axvline(
         mean_z + 2 * std_z,
         linestyle="-.",
@@ -155,7 +272,8 @@ def plot_probability_density(z, num_patches, path_to_figure):
         linewidth=0.75,
     )
     hist_patch = np.histogram(np.ravel(z, order="F"), bins=500, density=True)
-    plt.plot(hist_patch[1][:-1], hist_patch[0], label=f"Patches: {num_patches}")
+    plt.plot(hist_patch[1][:-1], hist_patch[0],
+             label=f"Patches: {num_patches}")
     plt.legend()
     plt.grid("show")
     plt.xlabel("h (mum)")
@@ -179,8 +297,10 @@ def plot_cumulative_distribution(z, num_patches, path_to_figure, probability=0.1
         color="k",
     )
     hist_patch = np.histogram(np.ravel(z, order="F"), bins=500, density=True)
-    cumulated_sum = (hist_patch[1][1] - hist_patch[1][0]) * np.cumsum(hist_patch[0])
-    plt.plot(hist_patch[1][:-1], cumulated_sum, label=f"Patches: {num_patches}")
+    cumulated_sum = (hist_patch[1][1] - hist_patch[1]
+                     [0]) * np.cumsum(hist_patch[0])
+    plt.plot(hist_patch[1][:-1], cumulated_sum,
+             label=f"Patches: {num_patches}")
     probabilities = np.linspace(0, 1, 100)
     quantiles = np.quantile(z_flat, probabilities)
     plt.plot(quantiles, probabilities, label=f"Scipy")
@@ -216,7 +336,8 @@ if __name__ == "__main__":
 
     n_iter = [1, 4, 16, 64]  # number of patches in total
     H = 0.75
-    surf_id = 1  # unique identifier for final "big" surface (I hope this help in database generation)
+    # unique identifier for final "big" surface (I hope this help in database generation)
+    surf_id = 1
 
     z_surf = {}
     surf_id = 0
@@ -238,10 +359,12 @@ if __name__ == "__main__":
         plot_probability_density(
             z_surf[n],
             num_patches=n,
-            path_to_figure=path_to_database / (final_surface_name + "_histogram.png"),
+            path_to_figure=path_to_database /
+            (final_surface_name + "_histogram.png"),
         )
         plot_cumulative_distribution(
             z_surf[n],
             num_patches=n,
-            path_to_figure=path_to_database / (final_surface_name + "_cdf.png"),
+            path_to_figure=path_to_database /
+            (final_surface_name + "_cdf.png"),
         )
