602bc69ffcdb41eb49056977da2544eb8c99cebd
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2025-12-05 15:30:46 +0100

    Evaluate surface statistics and blacken code

--- pydeep_sim/rough_surface/patches_generation.py
+++ pydeep_sim/rough_surface/patches_generation.py
@@ -7,7 +7,7 @@ from skspatial.objects import Plane, Points
 import numpy as np
 from matplotlib import pyplot as plt
 
-from pydeep_sim.rough_surface.rough_surface import RoughSurface
+from pydeep_sim.rough_surface.rough_surface import RoughSurface, random_postprocess
 
 
 def delete_content(directory):
@@ -48,10 +48,9 @@ def patches_generation(
     std0=0.09,
     verbose=False,
 ):
-
     """
     Docstring for patches_generation
-    
+
     :param H: Hurst exponenent
     :param iterations: overall number of local surfaces that will be stitched together
     :param surf_id: unique identifier for the global surface
@@ -63,7 +62,6 @@ def patches_generation(
     :param std0: initial standard deviation that determines grid points dislocation
     :param verbose: Description
     """
-
 
     path_to_patches = Path(path_to_patches)
     path_to_surface = Path(path_to_surface)
@@ -118,24 +116,23 @@ def patches_generation(
     #
     ############################################################################
 
-    dx = l/n_local
+    dx = l / n_local
     dy = dx
-    x = np.linspace(dx/2, l-dx/2, n_local)
-    y = np.linspace(dy/2, l-dy/2, n_local)
+    x = np.linspace(dx / 2, l - dx / 2, n_local)
+    y = np.linspace(dy / 2, l - dy / 2, n_local)
     X, Y = np.meshgrid(x, y)
 
     for k in range(iterations):
 
         # Rearrange surface points to perform plane fitting:
-        surface_array = Points(
-            np.vstack((X.ravel(), Y.ravel(), z_raw[k].ravel())).T)
+        surface_array = Points(np.vstack((X.ravel(), Y.ravel(), z_raw[k].ravel())).T)
         fitting_plane = Plane.best_fit(surface_array)
 
         # Coefficients of the fitting plane a*x+b*y+c*z+d == 0
         (a, b, c, d) = fitting_plane.cartesian()
 
         # Subtraction of fitting plane from original surface:
-        z_raw[k] = z_raw[k]-(-1/c*(a*X+b*Y+d))
+        z_raw[k] = z_raw[k] - (-1 / c * (a * X + b * Y + d))
 
         # By performing this operation, a small tilt remains, due to
         # linearization. This can be shown doing a new plane fitting, that
@@ -218,27 +215,31 @@ def patches_generation(
     th = 0.8
     for i in range(patches_per_side):
 
-        i_top = n_local*i
-        i_bottom = n_local*(i+1)-1
+        i_top = n_local * i
+        i_bottom = n_local * (i + 1) - 1
 
         if i_top > 0:
-            z_patch[i_top, :] = th * \
-                z_patch_raw[i_top, :] + (1-th)*z_patch_raw[i_top-1, :]
-        if i_bottom < n_local*patches_per_side-1:
-            z_patch[i_bottom, :] = th * \
-                z_patch_raw[i_bottom, :] + (1-th)*z_patch_raw[i_bottom+1, :]
+            z_patch[i_top, :] = (
+                th * z_patch_raw[i_top, :] + (1 - th) * z_patch_raw[i_top - 1, :]
+            )
+        if i_bottom < n_local * patches_per_side - 1:
+            z_patch[i_bottom, :] = (
+                th * z_patch_raw[i_bottom, :] + (1 - th) * z_patch_raw[i_bottom + 1, :]
+            )
 
     for j in range(patches_per_side):
 
-        j_left = n_local*j
-        j_right = n_local*(j+1)-1
+        j_left = n_local * j
+        j_right = n_local * (j + 1) - 1
 
         if j_left > 0:
-            z_patch[:, j_left] = th * \
-                z_patch_raw[:, j_left] + (1-th)*z_patch[:, j_left-1]
-        if j_right < n_local*patches_per_side-1:
-            z_patch[:, j_right] = th * \
-                z_patch_raw[:, j_right] + (1-th)*z_patch_raw[:, j_right+1]
+            z_patch[:, j_left] = (
+                th * z_patch_raw[:, j_left] + (1 - th) * z_patch[:, j_left - 1]
+            )
+        if j_right < n_local * patches_per_side - 1:
+            z_patch[:, j_right] = (
+                th * z_patch_raw[:, j_right] + (1 - th) * z_patch_raw[:, j_right + 1]
+            )
 
     # -5- ######################################################################
     #
@@ -278,8 +279,7 @@ def plot_probability_density(z, num_patches, path_to_figure):
         linestyle="--",
         color="k",
     )
-    plt.axvline(mean_z, linestyle=":", color="k",
-                linewidth=0.75, label="$\\mu$")
+    plt.axvline(mean_z, linestyle=":", color="k", linewidth=0.75, label="$\\mu$")
     plt.axvline(
         mean_z + 2 * std_z,
         linestyle="-.",
@@ -294,8 +294,7 @@ def plot_probability_density(z, num_patches, path_to_figure):
         linewidth=0.75,
     )
     hist_patch = np.histogram(np.ravel(z, order="F"), bins=500, density=True)
-    plt.plot(hist_patch[1][:-1], hist_patch[0],
-             label=f"Patches: {num_patches}")
+    plt.plot(hist_patch[1][:-1], hist_patch[0], label=f"Patches: {num_patches}")
     plt.legend()
     plt.grid("show")
     plt.xlabel("h (mum)")
@@ -319,10 +318,8 @@ def plot_cumulative_distribution(z, num_patches, path_to_figure, probability=0.1
         color="k",
     )
     hist_patch = np.histogram(np.ravel(z, order="F"), bins=500, density=True)
-    cumulated_sum = (hist_patch[1][1] - hist_patch[1]
-                     [0]) * np.cumsum(hist_patch[0])
-    plt.plot(hist_patch[1][:-1], cumulated_sum,
-             label=f"Patches: {num_patches}")
+    cumulated_sum = (hist_patch[1][1] - hist_patch[1][0]) * np.cumsum(hist_patch[0])
+    plt.plot(hist_patch[1][:-1], cumulated_sum, label=f"Patches: {num_patches}")
     probabilities = np.linspace(0, 1, 100)
     quantiles = np.quantile(z_flat, probabilities)
     plt.plot(quantiles, probabilities, label=f"Scipy")
@@ -381,12 +378,12 @@ if __name__ == "__main__":
         plot_probability_density(
             z_surf[k],
             num_patches=k,
-            path_to_figure=path_to_database /
-            (final_surface_name + "_histogram.png"),
+            path_to_figure=path_to_database / (final_surface_name + "_histogram.png"),
         )
         plot_cumulative_distribution(
             z_surf[k],
             num_patches=k,
-            path_to_figure=path_to_database /
-            (final_surface_name + "_cdf.png"),
+            path_to_figure=path_to_database / (final_surface_name + "_cdf.png"),
         )
+        surface_statistics = random_postprocess(z_surf[k], l)
+        print(surface_statistics)
