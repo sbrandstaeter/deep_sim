8aaa53198241d5ac31beb7957c84151d55625400
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2025-12-11 19:56:27 +0100

    Wrap functionality in callable

--- pydeep_sim/rough_surface/00_load_path.py
+++ pydeep_sim/rough_surface/00_load_path.py
@@ -6,113 +6,129 @@ import scipy
 from count_switches import count_switches
 
 # Surface resolution (points pe rside)
-n = 512
 
-# Surface lateral size
-L = 1.0
 
-# Grid cell area
-dx = L / n
-dA = dx**2
+def generate_surface_and_solve_pressure_driven_eff_area(
+    q1=16,
+    q2=64,
+    hurst=0.8,
+    n=512,
+    L=1.0,  # Surface lateral size
+    random_seed=1,
+    p_target=0.1,
+):
 
-# Surface generator
-sg = tm.SurfaceGeneratorFilter2D([n, n])
-sg.random_seed = 1
+    # Grid cell area
+    dx = L / n
+    dA = dx**2
 
-# Define spectrum
-sg.spectrum = tm.Isopowerlaw2D()
+    # Surface generator
+    sg = tm.SurfaceGeneratorFilter2D([n, n])
+    sg.random_seed = random_seed
 
-# Roll-off wavenumber (defines ratio between lateral size of the surface and longest wavelength. if lambda_0=2*pi/q0 ~ L the surface is not Gaussian)
-sg.spectrum.q0 = 16
-# Lowest wavenumber (defines longest wavelength of the surface as lambda_1 = 2*pi/q1)
-sg.spectrum.q1 = 16
-# Highest wavenumber (defines shortest wavelength of the surface as lambda_2 = 2*pi/q2)
-sg.spectrum.q2 = 64
-# Defines slope of the spectrum
-sg.spectrum.hurst = 0.8
+    # Define spectrum
+    sg.spectrum = tm.Isopowerlaw2D()
 
-# Generates the surface given the spectrum
-surface = sg.buildSurface()
+    # Roll-off wavenumber (defines ratio between lateral size of the surface and longest wavelength. if lambda_0=2*pi/q0 ~ L the surface is not Gaussian)
+    sg.spectrum.q0 = q1  # we set q0=q1 as it is not super important
+    # Lowest wavenumber (defines longest wavelength of the surface as lambda_1 = 2*pi/q1)
+    sg.spectrum.q1 = q1
+    # Highest wavenumber (defines shortest wavelength of the surface as lambda_2 = 2*pi/q2)
+    sg.spectrum.q2 = q2
+    # Defines slope of the spectrum
+    sg.spectrum.hurst = hurst
 
-rms_slope = tm.Statistics2D.computeSpectralRMSSlope(surface)
-# Comment not to normalize the RMSSLope
-surface /= rms_slope
+    # Generates the surface given the spectrum
+    surface = sg.buildSurface()
 
-# Should be equal to 1 if normalized
-rms_slope = tm.Statistics2D.computeSpectralRMSSlope(surface)
+    rms_slope = tm.Statistics2D.computeSpectralRMSSlope(surface)
+    # Comment not to normalize the RMSSLope
+    surface /= rms_slope
 
-# Creates the model
-model = tm.ModelFactory.createModel(tm.model_type.basic_2d, [L, L], [n, n])
+    # Should be equal to 1 if normalized
+    rms_slope = tm.Statistics2D.computeSpectralRMSSlope(surface)
 
-# Uncomment to solve equivalent non-periodic problem:
-# tm.ModelFactory.registerNonPeriodic(model, 'dcfft')
+    # Creates the model
+    model = tm.ModelFactory.createModel(tm.model_type.basic_2d, [L, L], [n, n])
 
-# Mechanical parameters
-model.E = 1.0
+    # Uncomment to solve equivalent non-periodic problem:
+    # tm.ModelFactory.registerNonPeriodic(model, 'dcfft')
 
-# Initialize the solver
-solver = tm.PolonskyKeerRey(model, surface, 1e-11)
+    # Mechanical parameters
+    model.E = 1.0
 
-# Uncomment to solve equivalent non-periodic problem:
-# solver.setIntegralOperator('dcfft')
+    # Initialize the solver
+    solver = tm.PolonskyKeerRey(model, surface, 1e-11)
 
-# Define load steps:
-p_target = 0.1
-loads = np.linspace(p_target / 10, p_target, 10)
+    # Uncomment to solve equivalent non-periodic problem:
+    # solver.setIntegralOperator('dcfft')
 
-# Solve for given load path:
-A_raw = np.empty((len(loads),))
-A_cor = np.empty_like(A_raw)
+    # Define load steps:
 
-Dmax = []
-Dmin = []
-Dmean = []
-for i, model in enumerate(load_path(solver, loads)):
+    loads = np.linspace(p_target / 10, p_target, 10)
 
-    print(i)
-    solver.solve(loads[i])
+    # Solve for given load path:
+    A_raw = np.empty((len(loads),))
+    A_cor = np.empty_like(A_raw)
 
-    # To compute the true displacement (for non-periodic problem), one needs to re-evaluate the displacement
-    # model.operators['dcfft'](model.traction, model.displacement)
+    Dmax = []
+    Dmin = []
+    Dmean = []
+    for i, model in enumerate(load_path(solver, loads)):
 
-    # Effective contact area (uncorrected)
-    A_raw[i] = dA * len(model.traction[model.traction > 0.0])
+        print(i)
+        solver.solve(loads[i])
 
-    # Perform correction of the area according to Yastrebov:
-    M = count_switches(model.traction)
-    Sd = M * dx
-    A_cor[i] = A_raw[i] - (np.pi - 1 + np.log(2)) / 24 * Sd * dx
+        # To compute the true displacement (for non-periodic problem), one needs to re-evaluate the displacement
+        # model.operators['dcfft'](model.traction, model.displacement)
 
-    Dmin.append(np.min(model.displacement))
-    Dmean.append(np.mean(model.displacement))
-    Dmax.append(np.max(model.displacement))
+        # Effective contact area (uncorrected)
+        A_raw[i] = dA * len(model.traction[model.traction > 0.0])
 
-A_ab = np.sqrt(2 * np.pi) * loads / rms_slope
-A_pr = scipy.special.erf(np.sqrt(2) * loads / rms_slope)
+        # Perform correction of the area according to Yastrebov:
+        M = count_switches(model.traction)
+        Sd = M * dx
+        A_cor[i] = A_raw[i] - (np.pi - 1 + np.log(2)) / 24 * Sd * dx
 
-fig, axs = plt.subplots(1, 2, figsize=(12, 5))
-axs[0].plot(loads / rms_slope, A_raw[:], "-v", label="n=512")
-axs[1].plot(loads / rms_slope, A_cor[:], "-^", label="n=512")
-axs[0].set_title("raw data")
-axs[1].set_title("corrected data")
-for i in range(2):
-    axs[i].plot(loads / rms_slope, A_ab, label="BGT")
-    axs[i].plot(loads / rms_slope, A_pr, label="Persson")
-    axs[i].set_xlabel("p0")
-    axs[i].set_ylabel("A/A0")
-    axs[i].grid(True)
-    axs[i].set_xlim([0.0, 0.15])
-    axs[i].set_ylim([0.0, 0.30])
-    axs[i].legend(loc="lower right")
-plt.savefig("plot_load.png")
+        Dmin.append(np.min(model.displacement))
+        Dmean.append(np.mean(model.displacement))
+        Dmax.append(np.max(model.displacement))
 
-fig, axs = plt.subplots(1, 1, figsize=(5, 5))
-axs.plot(loads / rms_slope, Dmin / np.max(surface), "-^", label="D_min")
-axs.plot(loads / rms_slope, Dmean / np.max(surface), "-^", label="D_mean")
-axs.plot(loads / rms_slope, Dmax / np.max(surface), "-<", label="D_max")
-axs.set_ylabel("D/zmax")
-axs.set_xlabel("p0")
-axs.grid(True)
-axs.legend(loc="upper right")
-axs.set_title("displacements")
-plt.savefig("plot_disp.png")
+    return surface, A_raw, A_cor, loads, rms_slope, Dmin, Dmean, Dmax
+
+
+if __name__ == "__main__":
+
+    surface, A_raw, A_cor, loads, rms_slope, Dmin, Dmean, Dmax = (
+        generate_surface_and_solve_pressure_driven_eff_area()
+    )
+    A_ab = np.sqrt(2 * np.pi) * loads / rms_slope
+    A_pr = scipy.special.erf(np.sqrt(2) * loads / rms_slope)
+
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+
+    axs[0].plot(loads / rms_slope, A_raw[:], "-v", label="n=512")
+    axs[1].plot(loads / rms_slope, A_cor[:], "-^", label="n=512")
+    axs[0].set_title("raw data")
+    axs[1].set_title("corrected data")
+    for i in range(2):
+        axs[i].plot(loads / rms_slope, A_ab, label="BGT")
+        axs[i].plot(loads / rms_slope, A_pr, label="Persson")
+        axs[i].set_xlabel("p0")
+        axs[i].set_ylabel("A/A0")
+        axs[i].grid(True)
+        axs[i].set_xlim([0.0, 0.15])
+        axs[i].set_ylim([0.0, 0.30])
+        axs[i].legend(loc="lower right")
+    plt.savefig("plot_load.png")
+
+    fig, axs = plt.subplots(1, 1, figsize=(5, 5))
+    axs.plot(loads / rms_slope, Dmin / np.max(surface), "-^", label="D_min")
+    axs.plot(loads / rms_slope, Dmean / np.max(surface), "-^", label="D_mean")
+    axs.plot(loads / rms_slope, Dmax / np.max(surface), "-<", label="D_max")
+    axs.set_ylabel("D/zmax")
+    axs.set_xlabel("p0")
+    axs.grid(True)
+    axs.legend(loc="upper right")
+    axs.set_title("displacements")
+    plt.savefig("plot_disp.png")
